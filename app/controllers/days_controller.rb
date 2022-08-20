38c7133fc833b0fdca8272e812cf2d650d7b93f4
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-20 21:58:19 +0200

    Restyling

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,11 +1,8 @@
 class DaysController < ApplicationController 
   before_action :set_day
   def update
-    p 'UPDATE!!'
-    p @day
         respond_to do |format|
       if @day.update(day_params)
-        format.turbo_stream 
         format.html { redirect_to root_url, notice: "Day was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
