0de078e126f8be326c72bb3225b91fa40326c0c0
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-21 22:51:11 +0200

    Add daily goal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   include GoalCalculator
   before_action :authenticate_user!
+
   def index
+    @today = Day
+      .where(date: Date.today.beginning_of_day..Date.today.end_of_day, user_id: current_user.id)
+      .select(:time)
+      .first
+
     @daily_goals = get_daily_goals
   end
 
